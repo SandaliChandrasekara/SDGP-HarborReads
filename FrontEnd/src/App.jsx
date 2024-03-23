d9ed4e7aa092ddd86481b1e8e922f61898745184
Sandaliii <sandalichandrasekarat20@gmail.com>
2024-03-23 22:37:56 +0530

    Implemented connection between backend and frontend

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -41,7 +41,7 @@ const App = () => {
 
   const handleSignIn = async (username, password) => {
     try {
-      const response = await fetch('https://ci-cd-be-2p3mwzqeaq-uc.a.run.app/auth/signin', {
+      const response = await fetch('https://harborreads-web-backend-y3b57u45qa-uc.a.run.app/auth/signin', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -74,7 +74,7 @@ const App = () => {
 
   const handleSignUp = async (username, email,password) => {
     try {
-      const response = await fetch('https://ci-cd-be-2p3mwzqeaq-uc.a.run.app/auth/signup', {
+      const response = await fetch('https://harborreads-web-backend-y3b57u45qa-uc.a.run.app/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -100,7 +100,7 @@ const App = () => {
   // Accessing user data
 const getUserData = async () => {
   try {
-    const response = await fetch('https://ci-cd-be-2p3mwzqeaq-uc.a.run.app/user/username', {
+    const response = await fetch('https://harborreads-web-backend-y3b57u45qa-uc.a.run.app/user/username', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
