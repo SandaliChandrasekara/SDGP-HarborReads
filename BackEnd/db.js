e412924162750c9b1655c0a31695e321d064c9ea
Sandaliii <sandalichandrasekarat20@gmail.com>
2024-03-23 22:27:19 +0530

    Dockerized harborreads app

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -9,10 +9,8 @@ mongoose.connect(uri)
 
 
 // const { MongoClient } = require('mongodb');
-
 // let dbConnection;
 // let uri = 'mongodb+srv://HarborReads:MongoDB%40HR12@cluster0.nhckanx.mongodb.net/harborreads?retryWrites=true&w=majority';
-
 // module.exports = {
 //     connectToDb: (cb) => {
 //         MongoClient.connect(uri)
